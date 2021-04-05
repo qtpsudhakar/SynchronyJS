ab4612f60d029aede3fb0ae35d9735086523003c
qtpsudhakar <sudhakar@qtpsudhakar.com>
2021-04-05 12:30:02 +0000

    for loop

---
+++ LoopsExample.js
@@ -0,0 +1,19 @@
+//print 10 for 5 times
+
+//for : executing a block of code for specific number of times
+//while : executing a block of code until condition failed
+
+for(let c=22;c<=26;c++){
+    console.log(10);
+}
+
+//print numbers from 1 to 10
+for(let c=10;c<=20;c++){ //c++ = c=c+1
+    console.log(c);
+}
+
+for(let c=11;c<=20;c+=2){ //c+=2 = c=c+2
+    console.log(c);
+}
+
+
