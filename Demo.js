f962000f9d129ff2a21bb71ff86a5682f4ae4d23
qtpsudhakar <sudhakar@qtpsudhakar.com>
2021-04-05 11:07:44 +0000

    update condition

--- Demo.js
+++ Demo.js
@@ -1,4 +1,4 @@
-"use strict" //force user to declare variable
+// "use strict" //force user to declare variable
 
 console.log("hello");
 
@@ -20,21 +20,15 @@ userName = "sudhakar";
 
 isExist = true;
 
+//array
+let userdata = ["selenium",4,2006];
+console.log(userdata[0]);
+console.log(userdata[1]);
+console.log(userdata[2]);
 
+// userdata = ["qtp",15,1998];
 
+//update an array
+userdata.push(10,20,30); //push will update array with new values
 
-
-
-
-
-
-
-
-
-
-
-
- let x1 = 1000
-
- console.log(x1);
- 
+console.log(userdata);
