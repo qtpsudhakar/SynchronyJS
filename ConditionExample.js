3b65b652df34fff5c62078fb92efd0a3a5e18eb3
qtpsudhakar <sudhakar@qtpsudhakar.com>
2021-04-05 11:25:10 +0000

    Condition1

---
+++ ConditionExample.js
@@ -0,0 +1,23 @@
+// find the given number is odd or even
+n = 11
+console.log(n%2);
+
+console.log(n%2==0);
+
+if(n%2==1){ //if block
+    console.log("odd number");
+}
+else{ //else block
+    console.log("even number");
+}
+
+// + : adition
+// - : sub
+// * : mul
+// / : div
+// % : mod
+// = : assign
+// == : compare data
+// === : compare data and type
+// < : less than
+// > : greater than
