9efea40cb751dd6746d74c1175324c1b0328d12c
qtpsudhakar <sudhakar@qtpsudhakar.com>
2021-04-05 10:31:33 +0000

    First comit

--- Demo.js
+++ Demo.js
@@ -1,1 +1,40 @@
+"use strict" //force user to declare variable
 
+console.log("hello");
+
+//assign to value to x
+
+let x=10;
+let y=20;
+z=x+y;
+
+console.log(z);
+
+x=100;
+y=200;
+z=x-y;
+
+console.log(z);
+
+userName = "sudhakar";
+
+isExist = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ let x1 = 1000
+
+ console.log(x1);
+ 
