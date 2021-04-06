d464ed3a935edb96e3093adeb7df95a57de98c1d
qtpsudhakar <sudhakar@qtpsudhakar.com>
2021-04-06 09:01:15 +0000

    loop

--- LoopsExample.js
+++ LoopsExample.js
@@ -3,17 +3,22 @@
 //for : executing a block of code for specific number of times
 //while : executing a block of code until condition failed
 
+//print number 10 for 5 times
 for(let c=22;c<=26;c++){
     console.log(10);
 }
 
 //print numbers from 1 to 10
-for(let c=10;c<=20;c++){ //c++ = c=c+1
+for(let c=1;c<=10;c++){ //c++ = c=c+1
     console.log(c);
 }
 
+//print odd numbers between 10 and 20
 for(let c=11;c<=20;c+=2){ //c+=2 = c=c+2
     console.log(c);
 }
 
-
+//print numbers from 10 to 1
+for(let i=10;i>=1;i--){
+    console.log(i);    
+}
