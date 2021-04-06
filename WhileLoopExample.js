72c02b07a564abbc0d77da51cd6bd169af17f8de
qtpsudhakar <sudhakar@qtpsudhakar.com>
2021-04-06 11:13:21 +0000

    all loops

---
+++ WhileLoopExample.js
@@ -0,0 +1,16 @@
+let n= 1;
+//while loop execute zero or more times until condition fail
+//while checks condition and execute block
+
+//do while execute block and checks condition
+//do while executes one or more times until condition fail
+
+while (n<10) { //executes until condition fail
+    console.log("execute something"); 
+    n++;
+}
+
+do{
+    console.log("execute something"); 
+    n++;
+}while(n<10)
