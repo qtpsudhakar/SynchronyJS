7d4c1b6dc001621b396c511434d8476368815353
qtpsudhakar <sudhakar@qtpsudhakar.com>
2021-04-07 10:29:59 +0000

    rest

--- PassingMultipleValues.js
+++ PassingMultipleValues.js
@@ -17,4 +17,16 @@ function demoAddAll(n) {
 
 let arr=[10,20,30,40,50];
 console.log(demoAddAll(arr));
+console.log(demoAddAll([10,20,30,40]));
 
+function demoAddAll2(...n) { //rest parameter : accepts any number of values
+    let out = 0;
+    for(let i=0;i<n.length;i++){
+        out=out+n[i];
+    }
+    return out;
+}
+
+console.log(demoAddAll2(2,4,1,5,6));
+
+console.log(demoAddAll2(...arr)); //spread parameter
